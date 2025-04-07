dbaeac41410dc0c64fb1e648f2431dc70d0cb947
E-G-L\esnuguel <tr374661@uaeh.edu.mx>
2025-04-06 20:36:29 -0600

    leectura de datos

--- Jerarquico.py
+++ Jerarquico.py
@@ -1,1 +1,18 @@
-#Angel y dios
+import pandas as pd
+from scipy.cluster.hierarchy import linkage, dendrogram
+import matplotlib.pyplot as plt
+
+# Lectura de datos
+
+df = pd.read_csv("Datos/ethylene_CO.txt", delim_whitespace=True, header=None) #Lee el txt y lo convierte a cvs
+# Cambia el nombre de las columnas
+columnas = ["Tiempo (s)", "CO (ppm)", "Etileno (ppm)"] + [f"Sensor {i+1}" for i in range(16)]
+df.columns = columnas
+#No es necesario solo lo convierte a cvs de manera externa
+#df.to_csv("archivo_convertido.csv", index=False)
+print(df.head())
+
+# Algoritmo de agrupamiento jerárquico
+df_sample = df.sample(n=10500)
+Z = linkage(df_sample, method='ward')
+print(Z)
